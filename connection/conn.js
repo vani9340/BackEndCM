ab8dde9c008c86d17091bd66446477c1002377ff
vani9340 <sg5428702@gmail.com>
2023-03-31 14:03:29 +0530

    change

--- connection/conn.js
+++ connection/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', false)
-const uri = "mongodb+srv://shivani:shivani@cluster0.6rnxqg5.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://shivanigupta777:shivanigupta777@cluster0.tzq1ity.mongodb.net/?retryWrites=true&w=majority"
 async function getConnection() {
     await mongoose.connect(uri, (err)=>{
         if(err){
